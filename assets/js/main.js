e576399f370543ffa7d7aea92e28f92c9919aa95
E024664 <E024664@esoft.academy>
2024-03-14 01:34:16 +0530

    feat: calculating cng cost

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,21 @@
-const firstName = prompt("What is your first name?")
-const lastName = prompt("What is your last name?")
+const tripDistanceEl = document.getElementById("trip-distance");
+const fuelEfficiencyEl = document.getElementById("fuel-efficiency");
+const fuelCostEl = document.getElementById("fuel-cost");
+const calculateBtnEl = document.getElementById("calculateBtn");
 
-const guestNameTxt = document.getElementById("guestName")
+calculateBtnEl.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (tripDistanceEl.value === "") {
+    alert("Enter Trip Distance value");
+  } else {
+    const distanceKM = tripDistanceEl.value;
+    const fuelEfficiency = fuelEfficiencyEl.value;
+    const fuelCost = fuelCostEl.value;
 
-if(firstName && lastName) {
-    guestNameTxt.innerText = firstName + ' ' + lastName + ","
-}
+    const result = (distanceKM / fuelEfficiency) * fuelCost;
 
+    const finalMsg = "Your fuel cost will be around: Rs." +result;
+    alert(finalMsg);
+    console.log(finalMsg);
+  }
+});
